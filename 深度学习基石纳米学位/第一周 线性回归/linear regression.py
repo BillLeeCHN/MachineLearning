872a08097fdb24a5202c8eafbb32a8d271a22377
BillLeeCHN <785348071@qq.com>
2017-02-19 12:40:44 +0800

    linear regression

---
+++ 深度学习基石纳米学位/第一周 线性回归/linear regression.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+class LinearRegressor:
+    def __init__(self, num_iteration, learning_rate, initial_m, initial_b):
+        self.num_iteration = num_iteration
+        self.learning_rate = learning_rate
+        self.m = initial_m
+        self.b = initial_b
+
+    def train(self, data):
+        for i in range(self.num_iteration):
+            gradient_m, gradient_b = self.computer_gradient(data)
+            self.m -= gradient_m * self.learning_rate
+            self.b -= gradient_b * self.learning_rate
+
+    def computer_gradient(self, data):
+        total_gradient_m = 0
+        total_gradient_b = 0
+        for i in range(len(data)):
+            x, y = data[i]
+            predict_y = self.predict(x)
+            gradient_m = 2 * x * (predict_y - y)
+            gradient_b = 2 * (predict_y - y)
+            total_gradient_m += gradient_m
+            total_gradient_b += gradient_b
+        average_gradient_m = total_gradient_m / float(len(data))
+        average_gradient_b = total_gradient_b / float(len(data))
+        return average_gradient_m, average_gradient_b
+
+    def predict(self, x):
+        return self.m * x + self.b
+
+    def computerMSE(self, data):
+        total_error = 0
+        for i in range(len(data)):
+            x, y = data[i]
+            predit_y = self.predict(x)
+            error = (y - predit_y)**2
+            total_error += error
+        average_error = total_error / float(len(data))
+        return average_error
+
+def run():
+    data = np.genfromtxt('data.txt', delimiter=',')
+
+    # Define Hyper parameter
+    num_iteration = 100
+    learning_rate = 0.0001
+    initial_m = 0
+    initial_b = 0
+
+    model = LinearRegressor(num_iteration, learning_rate, initial_m, initial_b)
+    print("Before training, error is {0}".format(model.computerMSE(data)))
+    model.train(data)
+    print("After training, eoor is {0}".format(model.computerMSE(data)))
+    print("After training, m is {0} and b is {1}".format(model.m, model.b))
+
+run()
