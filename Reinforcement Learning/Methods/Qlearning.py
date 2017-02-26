6c664f959e170ed6ac981e4c80c8c4db7f8e3afa
BillLeeCHN <785348071@qq.com>
2017-02-26 15:29:01 +0800

    正确的QLearning代码

--- Reinforcement Learning/Methods/Qlearning.py
+++ Reinforcement Learning/Methods/Qlearning.py
@@ -9,18 +9,11 @@ Created on Sat Feb 25 14:05:40 2017
 
 import numpy as np
 
+np.random.seed(2) # 每次产生的随机数不相同
+
 GAMMA = 0.8
-Q_table = np.zeros((6,6))
-EPISODES = 1000
-
-#Q_table = np.array([[   0. ,    0. ,    0. ,    0. ,  144.  ,   0. ],
-#                 [   0.  ,   0.   ,  0.  ,   0.   ,  0. ,  180. ],
-#                 [   0.  ,   0.  ,   0.  ,  64.  ,   0. ,    0. ],
-#                 [   0.  ,  80.  ,  51.2 ,   0.  , 144. ,    0. ],
-#                 [   0.  ,   0.   ,  0.  ,   0.  ,   0.  , 180. ],
-#                 [   0.   ,  0.   ,  0.  ,   0.  ,   0.  , 100. ]])
-
-
+Q_table = np.zeros([6,6])
+EPISODES = 3000
 
 # reward matrix R
 R_table = np.array([[-1,-1,-1,-1,0,-1],
@@ -30,20 +23,33 @@ R_table = np.array([[-1,-1,-1,-1,0,-1],
                   [0,-1,-1,0,-1,100],
                   [-1,0,-1,-1,0,100]])
 
-def choose_action(s, R_table, Q_table):
+# exploitation only, 仅利用，每一步的action按照Q表值大的选择
+def choose_action_exploitation(s, R_table, Q_table):
     rewards = R_table[s,:]
     positions = [] # 在状态s的情况下，可以走的路径为：positions
     for i in range(6):
-        if rewards[i] > -1:
+        if rewards[i] >= 0:
             positions.append(i)
     # 在Q表中处理
     values = []
     for i in range(np.size(positions)):
         values.append(Q_table[s, positions[i]])
+
     index_max = max_index(values)
     index = positions[index_max]
+
     return index
       
+# exploration only, 仅探索：每一步的action随机选择
+def choose_action_exploration(s, R_table, Q_table):
+    rewards = R_table[s,:]
+    positions = [] # 在状态s的情况下，可以走的路径为：positions
+    for i in range(6):
+        if rewards[i] >= 0:
+            positions.append(i)
+    # exploration only
+    index = np.random.choice(positions)
+    return index
      
 def max_index(values): # 返回数组最大数的所有索引
     count = np.size(values)
@@ -58,7 +64,7 @@ def max_index(values): # 返回数组最大数的所有索引
         return ind_max
     else:
         return np.random.choice(index)
-    np.indices
+
     
 
 def QLearning():
@@ -66,12 +72,16 @@ def QLearning():
         s = np.random.choice([0,1,2,3,4,5]) # 随机选择一个初始位置
         is_terminated = False
         while not is_terminated:
-            a = choose_action(s, Q_table,R_table)
+            # exploration only
+#            a = choose_action_exploration(s,R_table, Q_table)
+            # exploitation only
+            a = choose_action_exploitation(s,R_table, Q_table)
+            # update Q table  
+            Q_table[s,a] = R_table[s,a] + GAMMA * Q_table[a,:].max()
             
             if a == 5:
                 is_terminated = True
-            # update Q table  
-            Q_table[s,a] = R_table[s,a] + GAMMA * Q_table[a,:].max()
+                
             #update state
             s = a
     return Q_table
@@ -80,21 +90,22 @@ def QLearning():
 
 res = QLearning()
 print(res)
-#re = choose_action(5, R_table, Q_table)
-#print(re)
 
-#运行结果
+# result of exploration only
+#[[   0.    0.    0.    0.  400.    0.]
+# [   0.    0.    0.  320.    0.  500.]
+# [   0.    0.    0.  320.    0.    0.]
+# [   0.  400.  256.    0.  400.    0.]
+# [ 320.    0.    0.  320.    0.  500.]
+# [   0.  400.    0.    0.  400.  500.]]
+
+# result of exploitation only
 #[[   0.    0.    0.    0.  400.    0.]
 # [   0.    0.    0.    0.    0.  500.]
 # [   0.    0.    0.  320.    0.    0.]
-# [   0.  400.  256.    0.  400.    0.]
+# [   0.    0.    0.    0.  400.    0.]
 # [   0.    0.    0.    0.    0.  500.]
 # [   0.    0.    0.    0.    0.  500.]]
-
-
-
-
-
 
 
 
